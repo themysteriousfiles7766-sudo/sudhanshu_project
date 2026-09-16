5b38def576ea461e6318b1ff3108bfdd99398db0
themysteriousfiles7766-sudo <themysteriousfiles7766@gmail.com>
2026-09-16 12:39:14 +0530

    prograame

---
+++ project.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+r=pd.read_csv('abc.csv')
+print(r.head())
+print(r.describe())
+print(r.shape)
+print(r.all())
+print(r.columns)
+print(r.duplicated)
+print(r.dtypes)
+print(r.nunique)
+print(r.value_counts())
+
+print(np.mean)
+ls=r[(r["Region"]=="South") & (r["Category"]=="Technology")]
+print(ls)
